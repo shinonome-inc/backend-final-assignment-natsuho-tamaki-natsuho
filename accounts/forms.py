408140304fb435c9c4760cbe80c1e3bbe7a6dcbf
n-tamaki1506 <n._.tachi._.7141@au.com>
2024-06-11 01:43:42 +0900

    サインアップ機能ステップ2エラー

---
+++ accounts/forms.py
@@ -0,0 +1,10 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
+User = get_user_model()   # グローバル変数として扱う、ここで先に変数代入
+
+
+class SignupForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("username", "email")
--- accounts/views.py
+++ accounts/views.py
@@ -1,1 +1,23 @@
 # from django.shortcuts import render
+
+from django.contrib.auth import authenticate, login
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from .forms import SignupForm
+
+
+class SignupView(CreateView):
+    form_class = SignupForm
+    template_name = "accounts/signup.html"
+    success_url = reverse_lazy("tweets:home")  # リダイレクト先url貼付、success_url使用時遅延評価でurl逆引き
+
+    def from_valid(self, form):
+        response = super().form_valid(form)
+        username = form.cleaned_deta["username"]
+        password = form.cleaned_deta["password1"]   # passwordだとエラー、formのフィールドにpasswordというフィールドがない
+        user = authenticate(self.request, username=username, password=password)
+        login(self.request, user)
+        return response
+
+# エラー箇所あと一個？
