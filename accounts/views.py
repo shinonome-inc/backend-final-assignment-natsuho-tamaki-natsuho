bb37b95f88828268f466b835655c0fd0c37c70a9
n-tamaki1506 <n._.tachi._.7141@au.com>
2024-08-19 23:50:14 +0900

    ログイン・ログアウト機能の実装

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,11 @@
 # from django.shortcuts import render
 
+from django.conf import settings
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, TemplateView
 
 from .forms import SignupForm
 
@@ -10,7 +13,9 @@ from .forms import SignupForm
 class SignupView(CreateView):
     form_class = SignupForm
     template_name = "accounts/signup.html"
-    success_url = reverse_lazy("tweets:home")  # リダイレクト先url貼付、success_url使用時遅延評価でurl逆引き
+    success_url = reverse_lazy(
+        settings.LOGIN_REDIRECT_URL
+    )  # リダイレクト先url貼付、success_url使用時遅延評価でurl逆引き
 
     def form_valid(self, form):
         response = super().form_valid(form)
@@ -21,3 +26,15 @@ class SignupView(CreateView):
         user = authenticate(self.request, username=username, password=password)
         login(self.request, user)
         return response
+
+
+class UserProfileView(LoginRequiredMixin, TemplateView):
+    template_name = "accounts/user_profile.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        username = self.kwargs.get("username")
+        user = User.objects.get(username=username)
+        context["user"] = user
+        context["username"] = username
+        return context
