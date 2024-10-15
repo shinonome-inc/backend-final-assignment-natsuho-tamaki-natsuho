7425f1f191aecb40c70fc51f5280c9fece36ddb7
n-tamaki1506 <n._.tachi._.7141@au.com>
2024-10-15 19:55:12 +0900

    isort

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,6 @@
 from django.conf import settings
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.mixins import LoginRequiredMixin
-
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, TemplateView
 
